179b7595f91b5930a9cbad1c96c4a49f144bb638
bainbrid <bainbrid@users.noreply.github.com>
2021-10-07 16:19:14 +0200

    minor improvements, cosmetics

--- eff/eff.py
+++ eff/eff.py
@@ -19,43 +19,51 @@ def addOverflows(his):
     his.SetEntries(entries)
     return his
 
-def createPdf(his,canvas,eff=True):
+def createPdf(his,canvas,eff=True,logy=True,zmax=None):
     canvas.cd()
     his.Draw("TEXT COLZ")
     his.SetStats(0)
     if eff: 
         gStyle.SetPaintTextFormat("4.2f");
         his.SetMinimum(1.e-6)
-        his.SetMaximum(1.)
+        #his.SetMaximum(1.)
         his.SetMarkerSize(1.6)
     else: 
         gStyle.SetPaintTextFormat(".0f");
         his.SetMinimum(0.1)
+        if zmax is not None : his.SetMaximum(zmax)
         his.SetMarkerSize(1.)
-    canvas.SetLogz()
+    if logy : canvas.SetLogz()
     return canvas
 
 # numer
 print "numer"
-files_available = 79.
-files_processed = 77.
-numer_file = TFile('root/numer.root')
-numer_histo1 = numer_file.Get('numer_gen_lead')
-numer_histo2 = numer_file.Get('numer_gen_sub')
-numer_histo = numer_histo1.Clone("numer")
-numer_histo.Add(numer_histo2)
+numer_file = TFile('latest/numer.root')
+numer_histo_lead = numer_file.Get('numer_gen_lead')
+numer_histo_sub = numer_file.Get('numer_gen_sub')
+numer_histo = numer_histo_lead.Clone("numer")
+numer_histo.Add(numer_histo_sub)
 numer_histo = addOverflows(numer_histo)
 numer_histo.SetTitle("numer")
+numer_histo2 = numer_histo.Clone("numer_unweighted")
+numer_histo2.SetTitle("numer_unweighted")
+# scale
+files_available = 79.
+files_processed = 5.#77.
 numer_histo.Scale(files_available/files_processed)
 
 # denom
 print "denom"
-DAS_entries = 31585558.
-denom_file = TFile('root/denom.root')
-denom_histo = denom_file.Get('denom')
+denom_file = TFile('latest/denom.root')
+denom_histo = denom_file.Get('denom_sel')
 denom_histo = addOverflows(denom_histo)
+denom_histo.SetTitle("denom")
 denom_histo2 = denom_histo.Clone("denom_unweighted")
-denom_histo.Scale(DAS_entries/denom_histo.GetEntries())
+denom_histo2.SetTitle("denom_unweighted")
+# Scale
+DAS_entries = 31585558.
+entries = denom_file.Get('denom_nosel').GetEntries()
+denom_histo.Scale(DAS_entries/entries)
 
 # eff
 print "eff"
@@ -64,36 +72,62 @@ eff_histo.Divide(numer_histo,denom_histo)
 eff_histo.SetTitle("Analysis efficiency")
 #print "minimum  eff: ", eff_histo.GetMinimum(1.e-9)
 
+## debug
+#xbin = 2
+#ybin = 2
+#print "xbin",xbin,"ybin",ybin
+#print "denom:",
+#xlow = denom_histo.GetXaxis().GetBinLowEdge(xbin)
+#ylow = denom_histo.GetYaxis().GetBinLowEdge(xbin)
+#print "xlow",xlow,"ylow",ylow,
+#denom = denom_histo.GetBinContent(xbin,ybin)
+#unwei = denom_histo2.GetBinContent(xbin,ybin)
+#print "wei",denom,"unw",unwei,"ratio",denom/unwei
+#print "numer:",
+#xlow = numer_histo.GetXaxis().GetBinLowEdge(xbin)
+#ylow = numer_histo.GetYaxis().GetBinLowEdge(xbin)
+#print "xlow",xlow,"ylow",ylow,
+#numer = numer_histo.GetBinContent(xbin,ybin)
+#unwei = numer_histo2.GetBinContent(xbin,ybin)
+#print "wei",numer,"unw",unwei,"ratio",numer/unwei
+#print "eff",numer/denom
+
 # output
-eff_file = TFile('root/eff.root','RECREATE')
-numer_histo1.Write()
-numer_histo2.Write()
+eff_file = TFile('latest/eff.root','RECREATE')
+numer_histo_lead.Write()
+numer_histo_sub.Write()
 numer_histo.Write()
 denom_histo.Write()
 eff_histo.Write()
 
 numer_canvas = TCanvas()
-numer_canvas = createPdf(numer_histo,numer_canvas,eff=False)
-numer_canvas.SaveAs("plots/numer.pdf")
-
-numer_canvas1 = TCanvas()
-numer_canvas1 = createPdf(numer_histo1,numer_canvas1,eff=False)
-numer_canvas1.SaveAs("plots/numer1.pdf")
+numer_canvas = createPdf(numer_histo,numer_canvas,eff=False,zmax=1.e5)
+numer_canvas.SaveAs("latest/numer.pdf")
 
 numer_canvas2 = TCanvas()
-numer_canvas2 = createPdf(numer_histo2,numer_canvas2,eff=False)
-numer_canvas2.SaveAs("plots/numer2.pdf")
+numer_canvas2 = createPdf(numer_histo2,numer_canvas2,eff=False,zmax=2.e3)
+numer_canvas2.SaveAs("latest/numer_unweighted.pdf")
+
+numer_canvas_lead = TCanvas()
+numer_canvas_lead = createPdf(numer_histo_lead,numer_canvas_lead,eff=False)
+numer_canvas_lead.SaveAs("latest/numer_lead.pdf")
+
+numer_canvas_sub = TCanvas()
+numer_canvas_sub = createPdf(numer_histo_sub,numer_canvas_sub,eff=False)
+numer_canvas_sub.SaveAs("latest/numer_sub.pdf")
 
 denom_canvas = TCanvas()
-denom_canvas = createPdf(denom_histo,denom_canvas,eff=False)
-denom_canvas.SaveAs("plots/denom.pdf")
+denom_canvas = createPdf(denom_histo,denom_canvas,eff=False,zmax=5.e6)
+denom_canvas.SaveAs("latest/denom.pdf")
 
 denom_canvas2 = TCanvas()
-denom_canvas2 = createPdf(denom_histo2,denom_canvas2,eff=False)
-denom_canvas2.SaveAs("plots/denom_unweighted.pdf")
+denom_canvas2 = createPdf(denom_histo2,denom_canvas2,eff=False,zmax=2.e3)
+denom_canvas2.SaveAs("latest/denom_unweighted.pdf")
 
 eff_canvas = TCanvas()
-eff_canvas = createPdf(eff_histo,eff_canvas)
-eff_canvas.SaveAs("plots/eff.pdf")
+eff_canvas = createPdf(eff_histo,eff_canvas,logy=False)
+eff_canvas.SaveAs("latest/eff.pdf")
+eff_canvas = createPdf(eff_histo,eff_canvas,logy=True)
+eff_canvas.SaveAs("latest/eff_log.pdf")
 
 eff_file.Close()
--- eff/numer.C
+++ eff/numer.C
@@ -9,110 +9,112 @@
   t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_10_20.root");
   t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_110_120.root");
   t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_120_130.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_130_140.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_140_150.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_150_160.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_160_170.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_170_180.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_180_190.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_190_200.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_200_210.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_20_30.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_210_220.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_220_230.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_230_240.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_240_250.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_250_260.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_260_270.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_270_280.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_280_290.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_290_300.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_300_310.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_30_40.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_310_320.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_320_330.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_330_340.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_340_350.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_350_360.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_360_370.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_370_380.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_380_390.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_390_400.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_400_410.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_40_50.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_410_420.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_420_430.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_430_440.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_440_450.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_450_460.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_460_470.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_470_480.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_480_490.root");
-  //t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_490_500.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_500_510.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_50_60.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_510_520.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_520_530.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_530_540.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_540_550.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_550_560.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_560_570.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_570_580.root");
-  //t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_580_590.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_590_600.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_600_610.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_60_70.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_610_620.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_620_630.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_630_640.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_640_650.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_650_660.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_660_670.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_670_680.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_680_690.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_690_700.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_700_710.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_70_80.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_710_720.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_720_730.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_730_740.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_740_750.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_750_760.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_760_770.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_770_780.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_780_787.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_80_90.root");
-  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_90_100.root");
+
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_130_140.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_140_150.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_150_160.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_160_170.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_170_180.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_180_190.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_190_200.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_200_210.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_20_30.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_210_220.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_220_230.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_230_240.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_240_250.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_250_260.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_260_270.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_270_280.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_280_290.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_290_300.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_300_310.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_30_40.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_310_320.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_320_330.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_330_340.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_340_350.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_350_360.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_360_370.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_370_380.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_380_390.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_390_400.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_400_410.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_40_50.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_410_420.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_420_430.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_430_440.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_440_450.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_450_460.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_460_470.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_470_480.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_480_490.root");
+//  //t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_490_500.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_500_510.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_50_60.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_510_520.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_520_530.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_530_540.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_540_550.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_550_560.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_560_570.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_570_580.root");
+//  //t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_580_590.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_590_600.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_600_610.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_60_70.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_610_620.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_620_630.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_630_640.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_640_650.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_650_660.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_660_670.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_670_680.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_680_690.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_690_700.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_700_710.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_70_80.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_710_720.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_720_730.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_730_740.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_740_750.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_750_760.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_760_770.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_770_780.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_780_787.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_80_90.root");
+//  t->Add("/eos/cms/store/group/phys_bphys/cavalari/skimbig/BuToKee_v2_noreg_90_100.root");
 
   std::cout << "entries: " << t->GetEntries() << std::endl; // entries: TTree = 17050, TChain = 3137854
 
   std::string sel;
   sel += "bmatchMC==1";                                                         // GEN-match to signal
   sel += " && abs(k_svip3d) < 0.06 && fit_Bcos2D > 0.95";                       // Analysis pre-selection
-  sel += " && analysisBdtO > 8.";                                               // Analysis BT cut
+  sel += " && analysisBdtO > 8.";                                               // Analysis BDT cut
   sel += " && (mll_fullfit*mll_fullfit)>1.1 && (mll_fullfit*mll_fullfit)<6.25"; // Low q2 requirement
+  //sel += " && abs(tag_eta) < 1.2 && abs(probe_eta) < 1.2"; //@@ EXTRA: RECO eta selection
 
-  nbins = 13
-  
-  TH2F numer_reco("numer_reco","numer_reco",nbins, 0. ,nbins*1., nbins, 0. ,nbins*1.);
+  nbins = 13; width = 1.; //nbins = 3; width = 4.;
+
+  TH2F numer_reco("numer_reco","numer_reco",nbins, 0. ,nbins*width, nbins, 0. ,nbins*width);
   int n1 = t->Draw("tag_pt:probe_pt>>numer_reco",sel.c_str(),"goff");
-  std::cout << "processed: " << n1 << std::endl;
+  std::cout << "processed numer reco: " << n1 << std::endl;
 
-  TH2F numer_gen_all("numer_gen_all","numer_gen_all",nbins, 0. ,nbins*1., nbins, 0. ,nbins*1.);
+  TH2F numer_gen_all("numer_gen_all","numer_gen_all",nbins, 0. ,nbins*width, nbins, 0. ,nbins*width);
   int n2 = t->Draw("tag_ptMc:probe_ptMc>>numer_gen_all",sel.c_str(),"goff");
-  std::cout << "processed: " << n2 << std::endl;
+  std::cout << "processed numer gen_all: " << n2 << std::endl;
 
   std::string sel1 = "tag_ptMc>=probe_ptMc && " + sel;
-  TH2F numer_gen_lead("numer_gen_lead","numer_gen_lead",nbins, 0. ,nbins*1., nbins, 0. ,nbins*1.);
+  TH2F numer_gen_lead("numer_gen_lead","numer_gen_lead",nbins, 0. ,nbins*width, nbins, 0. ,nbins*width);
   int n3 = t->Draw("tag_ptMc:probe_ptMc>>numer_gen_lead",sel1.c_str(),"goff");
-  std::cout << "processed: " << n3 << std::endl;
+  std::cout << "processed numer gen_lead: " << n3 << std::endl;
 
   std::string sel2 = "tag_ptMc<probe_ptMc && " + sel;
-  TH2F numer_gen_sub("numer_gen_sub","numer_gen_sub",nbins, 0. ,nbins*1., nbins, 0. ,nbins*1.);
+  TH2F numer_gen_sub("numer_gen_sub","numer_gen_sub",nbins, 0. ,nbins*width, nbins, 0. ,nbins*width);
   int n4 = t->Draw("probe_ptMc:tag_ptMc>>numer_gen_sub",sel2.c_str(),"goff");
-  std::cout << "processed: " << n4 << std::endl;
+  std::cout << "processed numer gen_sub: " << n4 << std::endl;
   
-  TFile fw("root/numer.root","RECREATE");
+  TFile fw("latest/numer.root","RECREATE");
   numer_reco.Write();
   numer_gen_all.Write();
   numer_gen_lead.Write();
