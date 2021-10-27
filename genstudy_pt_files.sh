a612c3cc182e6cec23ce25750523e7ed2ccb2547
bainbrid <bainbrid@users.noreply.github.com>
2021-10-27 10:18:04 +0200

    list of files for denom using genstudy_pt

---
+++ genstudy_pt_files.sh
@@ -0,0 +1,1 @@
+dasgoclient --query="file dataset=/BuToKee_MufilterPt6_SoftQCDnonD_TuneCP5_13TeV-pythia8-evtgen/RunIIAutumn18MiniAOD-PUPoissonAve20_BParking_102X_upgrade2018_realistic_v15-v1/MINIAODSIM" >& files.txt &
