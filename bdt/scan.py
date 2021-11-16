1ea3157145216017cc441af9a61549e626551568
Rob Bainbridge <bainbrid@users.noreply.github.com>
2021-11-16 17:08:47 +0000

    tweaks

--- bdt/scan.py
+++ bdt/scan.py
@@ -9,9 +9,9 @@ mpl.use('pdf')
 from matplotlib import pyplot as plt
 from matplotlib import rc
 #.Allow for using TeX mode in matplotlib Figures
-rc('font',**{'family':'sans-serif','sans-serif':['Computer Modern Roman']})
-rc('text', usetex=True)
-plt.rcParams['text.latex.preamble']=[r"\usepackage{lmodern}"]
+#rc('font',**{'family':'sans-serif','sans-serif':['Computer Modern Roman']})
+#rc('text', usetex=False)
+#plt.rcParams['text.latex.preamble']=[r"\usepackage{lmodern}"]
 
 ratio=5.0/7.0
 fig_width_pt = 3*246.0  # Get this from LaTeX using \showthe\columnwidth
@@ -21,7 +21,7 @@ fig_width = fig_width_pt*inches_per_pt  # width in inches
 fig_height = fig_width*golden_mean      # height in inches
 fig_size =  [fig_width,fig_height]
 
-params = {'text.usetex' : True,
+params = {'text.usetex' : False,
         'axes.labelsize': 24,
         'font.size': 24,
         'legend.fontsize': 16,
@@ -30,7 +30,7 @@ params = {'text.usetex' : True,
         'font.family' : 'lmodern',
         'text.latex.unicode': True,
         'axes.grid' : True,
-        'text.usetex': True,
+        'text.usetex': False,
         'figure.figsize': fig_size}
 plt.rcParams.update(params)
 
@@ -62,34 +62,41 @@ if __name__ == "__main__":
           "eff_sum:",eff_sum.GetBinContent(xbin,ybin)
           )
 
+  fig, ax = plt.subplots()
+  etas=["eta<2.5","eta<1.2"]
   xaxis = [4,5,6,7,8,9,10,11,12,]
-  yaxis = []
-  for pt in [2]:
+  for name,pt,eta,color,his in [('pT-2GeV',2,0,"black",eff_sum),
+                                ('pT-2GeV',2,1,"blue",eff_cen),
+                                ('pT-5GeV',5,1,"green",eff_cen),
+                                ('pT-10GeV',10,1,"red",eff_cen),
+                                ]:
+    yaxis = []
+    print()
     for bdt in xaxis:
-      xbin = eff_sum.GetXaxis().FindBin(pt+1.e-3)
-      ybin = eff_sum.GetYaxis().FindBin(bdt+1.e-3)
-      eff = eff_sum.GetBinContent(xbin,ybin)
-      signal = eff * 35.
-      bkgd = funcs['pT-2GeV'](bdt)
-      significance = signal / np.sqrt(signal+bkgd)
-      yaxis.append(significance)
+      xbin = his.GetXaxis().FindBin(pt+1.e-3)
+      ybin = his.GetYaxis().FindBin(bdt+1.e-3)
+      eff = his.GetBinContent(xbin,ybin)
+      sig = eff * 40.
+      bkgd = funcs[name](bdt)
+      if eta==1 : bkgd /= 2.
+      #sigma = sig / np.sqrt(sig+bkgd)
+      sigma = np.sqrt( 2*((sig+bkgd)*np.log(1+sig/bkgd)-sig) )
+      yaxis.append(sigma)
       print("pt:",pt,
             "xbin:",xbin,
             "bdt cut:",bdt,
             "ybin:",ybin,
             "eff: {:.2f}".format(eff),
-            "signal: {:.2f}".format(signal),
+            "signal: {:.2f}".format(sig),
             "bkgd: {:.2f}".format(bkgd),
-            "significance: {:.2f}".format(significance)
+            "significance: {:.2f}".format(sigma)
             ) 
-    
-  fig, ax = plt.subplots()
-  ax.plot(xaxis,yaxis,'bo')#,mec='b',linestyle="",)
-  #ax.set_xlim(0.1,1.e5)
-  #ax.set_ylim(0.1,1.e5)
+    ax.plot(xaxis,yaxis,color=color, marker='o',label=name+", "+etas[eta])
+  ax.set_xlim(4,12)
+  ax.set_ylim(0.,10.)
   ax.set_xlabel('MVA cut')
   ax.set_ylabel('Significance')
   ax.legend(loc='best')
-  fig.savefig('scan.pdf'.format(name), bbox_inches='tight')
+  fig.savefig('scan.pdf',bbox_inches='tight')
 
 
